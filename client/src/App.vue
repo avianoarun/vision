c1acdb6e6fc9092d605a8a1f601cb0c04dfb1c1e
Arun Prasad <arunpx2015@email.iimcal.ac.in>
2020-05-08 14:03:31 +0530

    des added

--- client/src/App.vue
+++ client/src/App.vue
@@ -1,8 +1,10 @@
 <template>
   <div id="app">
   <div id="heading">
-  <img v-bind:src="'https://spaceplace.nasa.gov/templates/featured/sun/' + hello" width="200"/>
   <h1> Covid Face Mask Detection</h1>
+  </div>
+  <div id="description">
+  Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum
   </div>
   <div id="wrapper">
     <h1> Face Mask Detection</h1>
