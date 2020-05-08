e9631ea5e1cfc230f7164bd24c2855ffdf8b6618
Arun Prasad <arunpx2015@email.iimcal.ac.in>
2020-05-08 12:19:37 +0530

    Analytics is added

--- client/src/components/header.vue
+++ client/src/components/header.vue
@@ -1,1 +1,15 @@
+<template>
+<nav>
+<ul>
+<li><router-link to="/"></router-link>Home</li>
+<li><router-link to="/Analytics">Analytics</router-link></li>
+</ul>
 
+</nav>
+</template>
+<script>
+@import url('https://unpkg.com/vue-router@3.0.1/dist/vue-router.js');
+export default{}
+</script>
+<style scoped>
+</style>
