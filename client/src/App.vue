be035499a34932c3d4e7061071718a4de27b4743
Arun Prasad <arunpx2015@email.iimcal.ac.in>
2020-05-08 22:55:28 +0530

    Completed

--- client/src/App.vue
+++ client/src/App.vue
@@ -1,13 +1,13 @@
 <template>
   <div id="app">
   <div id="heading">
-  <h1> Covid Face Mask Detection</h1>
+  <h1> Covid Face Mask Detectron</h1>
  </div>
  <div id="description">
  The virus is spread at an alarming rate without the knowledge of the infected person. Covid-19 spreads when infected person coughs, sneezes or talks and droplets from their mouth or nose are launched into air and land in the “T Zone(Eyes, Nose, Mouth)” of people nearby. Therefore, wearing Mask is necessary step to be taken for safety of humanity as masks are the safety shield from the transfer of droplets. The usage of masks is the best and most effective method to stop the transfer of virus directly safeguarding self and others.
  </div>
  <div id="wrapper">
-    <h1> Face Mask Detection</h1>
+    <h1> Face Mask Detectron</h1>
     
         <Capture/>
         
